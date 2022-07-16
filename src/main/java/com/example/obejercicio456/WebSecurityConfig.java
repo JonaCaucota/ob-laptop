843e59049e583906bb981384aa69d4a5337d84ac
JonaCaucota <jonacau12@gmail.com>
2022-07-16 17:17:38 -0300

    con seguridad

---
+++ src/main/java/com/example/obejercicio456/WebSecurityConfig.java
@@ -0,0 +1,61 @@
+package com.example.obejercicio456;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/hola").permitAll().antMatchers("HolaPandita").hasRole("ADMIN")
+				.anyRequest().authenticated().and().formLogin().and().httpBasic();
+
+		return http.build();
+
+	}
+
+	@Bean
+	public HttpFirewall looseHttpFirewall() {
+		StrictHttpFirewall firewall = new StrictHttpFirewall();
+		firewall.setAllowBackSlash(true);
+		firewall.setAllowSemicolon(true);
+		return firewall;
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User
+				.withUsername("user")
+				.password(passwordEncoder().encode("123456"))
+				.roles("USER")
+				.build();
+		
+		UserDetails admin = User
+				.withUsername("admin")
+				.password(passwordEncoder().encode("password"))
+				.roles("USER", "ADMIN")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user, admin);
+		
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
